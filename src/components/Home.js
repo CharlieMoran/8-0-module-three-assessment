14c14738e2f8aac864a6b9a6e6f9262588643973
Charlie Moran <Carlosamoran18@gmail.com>
2021-12-16 19:04:07 -0500

    edited home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,19 +3,13 @@ import {withRouter} from "react-router";
 import "./Home.css";
 
 class Home extends Component {
-	constructor() {
-		super();
-		this.state = {
-		};
-	}
-
 render() {
 	return(
 	<div>	
-		<div></div>
 		<h1 className="welcome" id="welcome">Welcome To GhibliApp</h1>
-	</div>)
-}
+	</div>
+		)
+	}
 }
 
 export default withRouter(Home);
